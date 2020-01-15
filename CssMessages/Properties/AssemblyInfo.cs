0fbe32d1759fabaa1a2d343ee537b552a20e9497
Martijn Hendriks <hendriks@xvrsim.com>
2020-01-15 17:14:32 +0100

    first version of the CSS messages and their container class library

---
+++ CssMessages/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CssMessages")]
+[assembly: AssemblyDescription("Avro generated C# classes for all Common Simulation Space messages defined in CWA Building a Common Simulation Space")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("XVR Simulation BV")]
+[assembly: AssemblyProduct("CssMessages")]
+[assembly: AssemblyCopyright("Copyright © 2017-2020; XVR Simulation B.V., Delft, The Netherlands; Martijn Hendriks hendriks @ xvrsim.com; part of DRIVER WP923 Test-bed infrastructure project; licensed under the MIT license https://github.com/DRIVER-EU/test-bed/blob/master/LICENSE")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("60fcc141-a320-4ed0-af18-1ca2a7397b03")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.2020.0115")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,3 @@
+# common-simulation-space-messages
+
+Created within the DRIVER+ project and further defined inside the CEN Workshop Agreement "Building a Common Simulation Space", this is a small repository containing all CSS message schemas in Avro and a C# .NET Framework 4.6 class library with the generated classes belonging to those Avro schemas.
